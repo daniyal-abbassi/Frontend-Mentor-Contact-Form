c6d932c11bba606272149dc2b30d7c52cf79aa95
daniyal-abbassi <slimshady3753@protonmail.com>
2025-08-24 22:51:53 +0330

    feat: Add basic errorHandling

--- script-vue.js
+++ script-vue.js
@@ -1,10 +1,79 @@
+const { createApp, ref, reactive, onMounted } = Vue;
 
-const {createApp, ref, reactive} = Vue;
+createApp({
+  setup() {
+    //form inputs
+    const formData = reactive({
+      firstname: "",
+      lastname: "",
+      email: "",
+      query: "",
+      message: "",
+      consent: "",
+    });
+    const formErrors = reactive({
+      firstname: "",
+      lastname: "",
+      email: "",
+      query: "",
+      message: "",
+      consent: "",
+    });
+    //validation
 
-const app = createApp({
-    setup() {
-
+    function validateFirstName() {
+      if (formData.firstname.trim().length === 0) {
+        // errorState.value = true;
+        formErrors.firstname = "This field is required";
+      } else {
+        formErrors.firstname = "";
+      }
     }
-})
-
-app.mount('#app');
+    function validateLastName() {
+      if (formData.lastname.trim().length === 0) {
+        formErrors.lastname = "This field is required";
+      } else {
+        formErrors.lastname = "";
+      }
+    }
+    function validateEmail() {
+      if (formData.email.trim().length === 0) {
+        formErrors.email = "Please enter a valid email address";
+      } else {
+        formErrors.email = "";
+      }
+    }
+    function validateQuery() {
+          if (formData.query.trim().length === 0) {
+        formErrors.query = "Please select a query type";
+      } else {
+        formErrors.query = "";
+      }
+    }
+        function validateMessage() {
+          if (formData.message.trim().length === 0) {
+        formErrors.message = "This field is required";
+      } else {
+        formErrors.message = "";
+      }
+    }
+        function validateConsent() {
+          if (formData.consent.trim().length === 0) {
+        formErrors.consent = "To submit this form, please consent to being contacted";
+      } else {
+        formErrors.consent = "";
+      }
+    }
+    
+    return {
+      formData,
+      validateFirstName,
+      validateLastName,
+      validateEmail,
+      validateQuery,
+      validateMessage,
+      validateConsent,
+      formErrors,
+    };
+  },
+}).mount("#app");
