2d6a78d0442d3c338631ca5a88288483d93e092d
daniyal-abbassi <slimshady3753@protonmail.com>
2025-08-24 22:52:40 +0330

    fix: delete vur from filenames

--- script.js
+++ script.js
@@ -1,92 +1,79 @@
-//get elements
-const firstNameInput = document.getElementById("firstname");
-const lastNameInput = document.getElementById("lastname");
-const emailInput = document.getElementById("email");
-const messageInput = document.getElementById("message");
-const consentInput = document.getElementById("consent");
-const queryInputs = document.querySelectorAll('input[name="query"]');
-const queryError = document.getElementById("query-error");
-const consentError = document.getElementById("consent-error");
-const messageError = document.getElementById("message-error");
-const emailError = document.getElementById("email-error");
-const firstnameError = document.getElementById("firstname-error");
-const lastnameError = document.getElementById("lastname-error");
-const submitBtn = document.getElementById("submit-btn");
-const successToast = document.getElementById("success-message");
-const form = document.getElementById('form');
+const { createApp, ref, reactive, onMounted } = Vue;
 
-//timeout for toast
-let toastTimeout;
+createApp({
+  setup() {
+    //form inputs
+    const formData = reactive({
+      firstname: "",
+      lastname: "",
+      email: "",
+      query: "",
+      message: "",
+      consent: "",
+    });
+    const formErrors = reactive({
+      firstname: "",
+      lastname: "",
+      email: "",
+      query: "",
+      message: "",
+      consent: "",
+    });
+    //validation
 
-form.addEventListener('submit',(e)=>{
-  e.preventDefault();
-})
-//delete error message when type in firstname
-firstNameInput.addEventListener("change", (e) => {
-  if (firstNameInput.value) {
-    firstnameError.style.display = "none";
-  }
-});
-lastNameInput.addEventListener("change", () => {
-  if (lastNameInput.value) {
-    lastnameError.style.display = "none";
-  }
-});
-emailInput.addEventListener("change", () => {
-  if (emailInput.value) {
-    emailError.style.display = "none";
-  }
-});
-messageInput.addEventListener("change", () => {
-  if (messageInput.value) {
-    messageError.style.display = "none";
-  }
-});
-consentError.addEventListener("change", () => {
-  if (consentInput.checked) {
-    consentError.style.display = "none";
-  }
-});
-queryInputs.forEach((q) => {
-  q.addEventListener("change", () => {
-    if (q.checked) {
-      queryError.style.display = "none";
+    function validateFirstName() {
+      if (formData.firstname.trim().length === 0) {
+        // errorState.value = true;
+        formErrors.firstname = "This field is required";
+      } else {
+        formErrors.firstname = "";
+      }
     }
-  });
-});
-//add on cick function
-submitBtn.addEventListener("click", () => {
-  if (!firstNameInput.value) {
-    firstnameError.style.display = "block";
-  }
-  if (!lastNameInput.value) {
-    lastnameError.style.display = "block";
-  }
-  if (!emailInput.value) {
-    emailError.style.display = "block";
-  }
-  if (!messageInput.value) {
-    messageError.style.display = "block";
-  }
-  if (!consentInput.checked) {
-    consentError.style.display = "block";
-  }
-  if (!Array.from(queryInputs).some((q) => q.checked)) {
-    queryError.style.display = "block";
-  }
-   
-  if (
-    firstNameInput.value &&
-    lastNameInput.value &&
-    emailInput.value &&
-    messageInput.value &&
-    consentInput.checked &&
-    Array.from(queryInputs).some((q) => q.checked)
-  ) {
-    clearInterval(toastTimeout);
-    successToast.style.display = 'flex';
-    toastTimeout = setTimeout(()=>{
-      successToast.style.display = 'none';
-    },2000)
-  }
-});
+    function validateLastName() {
+      if (formData.lastname.trim().length === 0) {
+        formErrors.lastname = "This field is required";
+      } else {
+        formErrors.lastname = "";
+      }
+    }
+    function validateEmail() {
+      if (formData.email.trim().length === 0) {
+        formErrors.email = "Please enter a valid email address";
+      } else {
+        formErrors.email = "";
+      }
+    }
+    function validateQuery() {
+          if (formData.query.trim().length === 0) {
+        formErrors.query = "Please select a query type";
+      } else {
+        formErrors.query = "";
+      }
+    }
+        function validateMessage() {
+          if (formData.message.trim().length === 0) {
+        formErrors.message = "This field is required";
+      } else {
+        formErrors.message = "";
+      }
+    }
+        function validateConsent() {
+          if (formData.consent.trim().length === 0) {
+        formErrors.consent = "To submit this form, please consent to being contacted";
+      } else {
+        formErrors.consent = "";
+      }
+    }
+    
+    return {
+      formData,
+      validateFirstName,
+      validateLastName,
+      validateEmail,
+      validateQuery,
+      validateMessage,
+      validateConsent,
+      formErrors,
+    };
+  },
+}).mount("#app");
